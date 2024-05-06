98a615d1aef394e7b7d0061f558a779382518a5b
4joejoe <shantanuchaubey1@gmail.com>
2024-05-06 06:26:45 +0530

    Add virtual environment setup and runserver command to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+python3 -m venv env
+source env/bin/activate
+
 pip install -r req.txt
 
 python manage.py makemigrations
 python manage.py migrate
+
+# comment out if using on remote server
+python3 manage.py runserver
