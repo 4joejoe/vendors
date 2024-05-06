11389c08333e7421c9869fbf967339563b292d7e
4joejoe <shantanuchaubey1@gmail.com>
2024-05-06 11:11:21 +0530

    Update build.sh to include gunicorn command for deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-python3 -m venv env
-source env/bin/activate
+# python3 -m venv env
+# source env/bin/activate
 
 pip install -r req.txt
 
@@ -8,3 +8,4 @@ python manage.py migrate
 
 # comment out if using on remote server
 # python3 manage.py runserver
+gunicorn --bind 0.0.0.0:8000 backend.wsgi:application
